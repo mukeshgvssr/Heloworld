883cf3535fb0300be7f4d6da5b303a085cbd30dd
mukeshgvssr <mukesh.gadidasu@gmail.com>
2015-08-20 18:06:06 +0530

    Create HellowWorld.java

---
+++ HellowWorld.java
@@ -0,0 +1,14 @@
+/*
+ *  To write a java program to print "Hellow World"
+ */
+
+public class HellowWorld {
+
+	
+	public static void main(String[] args) {
+	  
+		System.out.println ("Hellow World !");
+
+	}
+
+}
